616511a3272e4b7f7603655657d50c517f983375
Andrés Hoyos <andres.hoyos@naranjax.com>
2025-01-17 21:02:02 -0300

    refactor: winner-checker tests

--- packages/test/models/winner.test.js
+++ packages/test/models/winner.test.js
@@ -1,244 +1,62 @@
 import { Board, X, O, WinnerChecker } from "@tic-tac-toe-chicago-school/models";
+
+const WINNING_COMBINATIONS = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9],  // Rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9],  // Columns
+    [1, 5, 9], [3, 5, 7]              // Diagonals
+];
+
+const NON_WINNING_COMBINATIONS = [
+    [1, 2], [4, 5], [7, 8],  // Partial Rows
+    [1, 4], [2, 5], [3, 6],  // Partial Columns
+    [1, 5], [3, 5]           // Partial Diagonals
+];
+
+const testWinningCombination = (token, positions) => {
+    const winnerChecker = new BuilderWinnerCheckerTest(token);
+    positions.forEach(pos => winnerChecker.withToken(pos));
+    expect(winnerChecker.build().isTicTacToe()).toBe(true);
+}
+
+const testNonWinningCombination = (token, positions) => {
+    const winnerChecker = new BuilderWinnerCheckerTest(token);
+    positions.forEach(pos => winnerChecker.withToken(pos));
+    expect(winnerChecker.build().isTicTacToe()).toBe(false);
+}
+
 [new X(), new O()].forEach((token) => {
     describe("winner-checker", () => {
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(2))
-                .withToken(position(3))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
+        WINNING_COMBINATIONS.forEach((combination) => {
+            it(`Given a Board with a winner (${combination}), when calling isTicTacToe, then should return true`, () => {
+                testWinningCombination(token, combination);
+            });
         });
 
-        it("Given a Board without a winner, when calling isTicTacToe, then should return false", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(2))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(false);
-        });
-
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(5))
-                .withToken(position(9))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board without a winner, when calling isTicTacToe, then should return false", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(5))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(false);
-        });
-
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(4))
-                .withToken(position(7))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board without a winner, when calling isTicTacToe, then should return false", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(4))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(false);
-        });
-
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(2))
-                .withToken(position(5))
-                .withToken(position(8))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board without a winner, when calling isTicTacToe, then should return false", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(2))
-                .withToken(position(5))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(false);
-        });
-
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(3))
-                .withToken(position(6))
-                .withToken(position(9))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board without a winner, when calling isTicTacToe, then should return false", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(3))
-                .withToken(position(6))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(false);
-        });
-
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(3))
-                .withToken(position(5))
-                .withToken(position(7))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board without a winner, when calling isTicTacToe, then should return false", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(3))
-                .withToken(position(5))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(false);
-        });
-
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(4))
-                .withToken(position(5))
-                .withToken(position(6))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board without a winner, when calling isTicTacToe, then should return false", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(4))
-                .withToken(position(5))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(false);
-        });
-
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(3))
-                .withToken(position(6))
-                .withToken(position(9))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board with a winner, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(7))
-                .withToken(position(8))
-                .withToken(position(9))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board without a winner, when calling isTicTacToe, then should return false", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(7))
-                .withToken(position(8))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(false);
+        NON_WINNING_COMBINATIONS.forEach((combination) => {
+            it(`Given a Board without a winner (${combination}), when calling isTicTacToe, then should return false`, () => {
+                testNonWinningCombination(token, combination);
+            });
         });
 
         it("Given an empty Board, when calling isTicTacToe, then should return false", () => {
             const winnerChecker = new BuilderWinnerCheckerTest(token).build();
             expect(winnerChecker.isTicTacToe()).toBe(false);
         });
-
-        it("Given a Board with a winner in the first row, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(2))
-                .withToken(position(3))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board with a winner in the second row, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(4))
-                .withToken(position(5))
-                .withToken(position(6))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board with a winner in the third row, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(7))
-                .withToken(position(8))
-                .withToken(position(9))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board with a winner in the first column, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(4))
-                .withToken(position(7))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board with a winner in the second column, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(2))
-                .withToken(position(5))
-                .withToken(position(8))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board with a winner in the third column, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(3))
-                .withToken(position(6))
-                .withToken(position(9))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board with a winner in the first diagonal, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(1))
-                .withToken(position(5))
-                .withToken(position(9))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
-
-        it("Given a Board with a winner in the second diagonal, when calling isTicTacToe, then should return true", () => {
-            const winnerChecker = new BuilderWinnerCheckerTest(token)
-                .withToken(position(3))
-                .withToken(position(5))
-                .withToken(position(7))
-                .build();
-            expect(winnerChecker.isTicTacToe()).toBe(true);
-        });
     });
 });
 
 class BuilderWinnerCheckerTest {
-  constructor(token) {
-    this._token = token;
-    this._board = new Board();
-  }
+    constructor(token) {
+        this._token = token;
+        this._board = new Board();
+    }
 
-  withToken(position) {
-    this._board.putToken(position, this._token);
-    return this;
-  }
+    withToken(position) {
+        this._board.putToken(position, this._token);
+        return this;
+    }
 
-  build() {
-    return new WinnerChecker(this._board);
-  }
+    build() {
+        return new WinnerChecker(this._board);
+    }
 }
-
-const position = (position) => position;
