5b973d67d3b2d90c8dbfedb204a0d954f8465ff7
Andrés Hoyos <andres.hoyos@naranjax.com>
2025-01-22 21:37:49 -0300

    feat: added turn class

--- packages/test/jest.config.js
+++ packages/test/jest.config.js
@@ -1,8 +1,3 @@
 export default {
-    collectCoverage: true,
-    collectCoverageFrom: [
-        "./node_modules/@tic-tac-toe-chicago-school/models/*.js", 
-        "!**/node_modules/**",
-        "!**/tests/**",
-    ]
+    collectCoverage: false,
   };
---
+++ packages/test/models/turn.test.js
@@ -0,0 +1,57 @@
+import { Board, Player, Turn } from "@tic-tac-toe-chicago-school/models";
+
+describe('Turn', () => {
+    let board, players, turn;
+
+    const createBoard = () => new Board();
+    const createPlayers = (board) => [new Player(board), new Player(board)];
+    const createTurn = (players) => new Turn(players);
+
+    beforeEach(() => {
+        board = createBoard();
+        players = createPlayers(board);
+        turn = createTurn(players);
+    });
+
+    describe('Given players are not provided', () => {
+        it('Then it should throw an error', () => {
+            expect(() => new Turn()).toThrow('Players are required.');
+        });
+    });
+
+    describe('Given players are provided', () => {
+        beforeEach(() => {
+            players = createPlayers(board);
+            turn = createTurn(players);
+        });
+
+        it('Then it should assign a token to each player upon initialization', () => {
+            players.forEach((player, index) => {
+                player.moveTokenFromRackToBoard(index + 1);
+                expect(board.getTokenOn(index + 1).isNull()).toBe(false);
+            });
+            expect(board.getTokenOn(1).isEqual(board.getTokenOn(2))).toBe(false);
+        });
+
+        describe('When it is a player\'s turn', () => {
+            it('Then it should allow the correct player to play on their turn', () => {
+                let currentPlayer = turn.nextPlayer();
+                currentPlayer.moveTokenFromRackToBoard(1);
+                expect(board.getTokenOn(1).isNull()).toBe(false);
+
+                currentPlayer = turn.nextPlayer();
+                currentPlayer.moveTokenFromRackToBoard(2);
+                expect(board.getTokenOn(2).isNull()).toBe(false);
+
+                expect(board.getTokenOn(1).isEqual(board.getTokenOn(2))).toBe(false);
+            });
+
+            it('Then it should allow querying the current player', () => {
+                const player = turn.nextPlayer();
+                expect(player).toStrictEqual(turn.currentPlayer());
+            });
+        });
+
+
+    });
+});
